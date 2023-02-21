34fdc47e3a7affeb7e987d5c4620af774d865a80
suzukikoutarou <kotarosz727@gmail.com>
2023-02-21 09:12:27 +0900

    feat: create users DTO and validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, MaxLength, IsPhoneNumber } from "class-validator";
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @MaxLength(255)
+  name: string;
+
+  @IsNotEmpty()
+  age: number;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @MaxLength(20)
+  @IsPhoneNumber("JP")
+  phone: string;
+}
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,21 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { PartialType } from "@nestjs/mapped-types";
+import { CreateUserDto } from "./create-user.dto";
+import { IsEmail, IsNotEmpty, MaxLength, IsPhoneNumber } from "class-validator";
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsNotEmpty()
+  @MaxLength(255)
+  name: string;
+
+  @IsNotEmpty()
+  age: number;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @MaxLength(20)
+  @IsPhoneNumber("JP")
+  phone: string;
+}
