a06497529edf8b0f873fbbfca8954da5b47b10f9
suzukikoutarou <kotarosz727@gmail.com>
2023-03-05 11:57:51 +0900

    feat: revise Kanban Entity

--- src/kanbans/entities/kanban.entity.ts
+++ src/kanbans/entities/kanban.entity.ts
@@ -1,8 +1,24 @@
 import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
 import { User } from '../../users/entities/user.entity';
 
+type KanbanStatus = {
+  0: 'todo';
+  1: 'doing';
+  2: 'done';
+};
+
+type KanbanStatusKey = keyof KanbanStatus;
+
+interface IKanban {
+  id: string;
+  name: string;
+  description: string;
+  status: KanbanStatusKey;
+  user: Pick<User, 'id'> | null;
+}
+
 @Entity('kanbans')
-export class Kanban {
+export class Kanban implements IKanban {
   @PrimaryGeneratedColumn('uuid')
   id: string;
   @Column({ type: 'varchar', length: 255 })
@@ -10,7 +26,7 @@ export class Kanban {
   @Column({ type: 'varchar', length: 255 })
   description: string;
   @Column('tinyint')
-  status: number;
+  status: KanbanStatusKey;
   @ManyToOne(() => User, (user) => user.kanbans, { nullable: true })
   user: User;
 }
