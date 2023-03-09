e9eed20e0048594155a353dd5091653850b99ed0
suzukikoutarou <kotarosz727@gmail.com>
2023-03-09 20:55:55 +0900

    feat: revise KanbanController test

--- src/kanbans/kanbans.controller.spec.ts
+++ src/kanbans/kanbans.controller.spec.ts
@@ -38,4 +38,59 @@ describe('KanbansController', () => {
     const result = await controller.findAll();
     expect(result).toEqual(kanbans);
   });
+
+  it('should return a kanban', async () => {
+    const kanban = new Kanban('1', 'タイトル1', 0);
+    jest.spyOn(controller, 'findOne').mockImplementation(async (id: string) => {
+      return {
+        id: id,
+        ...kanban,
+      };
+    });
+    const result = await controller.findOne('find_one_test');
+    expect(result).toEqual({ id: 'find_one_test', ...kanban });
+  });
+
+  it('should create kanban', async () => {
+    const kanban = new Kanban('1', 'タイトル1', 0);
+    jest
+      .spyOn(controller, 'create')
+      .mockImplementation(async (kanban: Kanban) => {
+        return {
+          id: 'create_test',
+          ...kanban,
+        };
+      });
+    const result = await controller.create(kanban);
+    expect(result).toEqual({ id: 'create_test', ...kanban });
+  });
+
+  it('should update kanban', async () => {
+    const kanban = new Kanban('1', 'タイトル1', 0);
+    jest
+      .spyOn(controller, 'update')
+      .mockImplementation(async (id: string, kanban: Kanban) => {
+        return {
+          id: id,
+          ...kanban,
+        };
+      });
+    const result = await controller.update('update_test', kanban);
+    expect(result).toEqual({ id: 'update_test', ...kanban });
+  });
+
+  it('should delete kanban', async () => {
+    const deleted = {
+      deleted: true,
+      message: '削除しました',
+    };
+    jest.spyOn(controller, 'remove').mockImplementation(async () => {
+      return {
+        deleted: true,
+        message: '削除しました',
+      };
+    });
+    const result = await controller.remove('remove_test');
+    expect(result).toEqual(deleted);
+  });
 });
